332be75d797a1b73c7bf05581decc627fde5163f
Josh Klina <joshua.klina@gmail.com>
2016-01-25 22:51:09 -0500

    making more controllers easier to override

--- app/controllers/exposition/posts_controller.rb
+++ app/controllers/exposition/posts_controller.rb
@@ -1,13 +1,5 @@
 module Exposition
   class PostsController < ApplicationController
-    def index
-      @posts = Post.published.sorted_by_published_date.
-        page(params[:page]).
-        per(10)
-    end
-
-    def show
-      @post = Post.published.find_by_slug!(params[:id])
-    end
+    include Concerns::Controller::PostsController
   end
 end
